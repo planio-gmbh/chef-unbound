fcd3c45f35bb61eeb48c20751e0182f7e13d67fb
Holger Just <hjust@meine-er.de>
2014-04-07 16:43:50 +0200

    Suggest runit as a dependency

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 recipe "unbound::default", "Installs unbound and sets up configuration files"
+
+suggests 'runit'
